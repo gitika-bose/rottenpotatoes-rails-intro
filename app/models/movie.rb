5afd4cb9dbb52e07c745c675187677130e4bb469
Gitika Bose <gitikabose@dyn-129-236-224-235.dyn.columbia.edu>
2021-02-05 23:47:09 -0500

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings()
+    @all_ratings = ['G','PG','PG-13','R']
+    return @all_ratings
+  end
+
+  def self.with_ratings(ratings)
+    if ratings == [] or ratings == nil
+      where(rating: ['G','PG','PG-13','R'])
+    else
+      where(rating: ratings)
+    end
+  end
 end
